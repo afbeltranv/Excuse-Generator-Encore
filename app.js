39db9e4623b671e15a5a770dc4125ce7ce060ea8
afbeltranv <afbeltranv@gmail.com>
2023-04-04 02:11:16 +0000

    Excuse Generator Terminado en Boilerplate de HTML5

---
+++ app.js
@@ -0,0 +1,21 @@
+window.onload = () => {
+  document.querySelector("#one").innerHTML = excuse;
+};
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+let excuse =
+  who[Math.floor(Math.random() * 3 + 1)] +
+  " " +
+  action[Math.floor(Math.random() * 3 + 1)] +
+  " " +
+  what[Math.floor(Math.random() * 2 + 1)] +
+  " " +
+  when[Math.floor(Math.random() * 4 + 1)];
